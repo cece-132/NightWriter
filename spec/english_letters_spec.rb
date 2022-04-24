6c16fd4fcb6c343fb33d58bf8497756f10dfc872
Candace Eckels <candace.eckels@gmail.com>
2022-04-24 13:14:38 -0600

    Readability

--- spec/english_letters_spec.rb
+++ spec/english_letters_spec.rb
@@ -14,13 +14,14 @@ RSpec.describe English do
   end
 
   it "has letters in the alpha attribute" do
-    expected = {  A: "A", B: "B", C: "C", D: "D", E: "E", F: "F", G: "G", H: "H",
+    expected = {A: "A", B: "B", C: "C", D: "D", E: "E", F: "F", G: "G", H: "H",
                 I: "I", J: "J", K: "K", L: "L", M: "M", N: "N", O: "O", P: "P",
                 Q: "Q", R: "R", S: "S", T: "T", U: "U", V: "V", W: "W", X: "X",
                 Y: "Y", Z: "Z"}
-                
+
     expect(@english.alpha).to eq(expected)
   end
+
 end
 #
 # def alpha_hash
