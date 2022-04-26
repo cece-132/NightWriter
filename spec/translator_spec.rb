63194c812b7230df12497ef50701bef22e23fc23
Candace Eckels <candace.eckels@gmail.com>
2022-04-26 16:41:47 -0600

    Translate tests 1 and 2 pass

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -3,29 +3,17 @@ require './lib/translator'
 
 RSpec.describe Translator do
   before :each do
-    @translate_to_braille = Translator.new
+    @translate = Translator.new('message_reader')
   end
 
   it "exists" do
-    expect(@translate_to_braille).to be_a(Translator)
+    expect(@translate).to be_a(Translator)
   end
 
-  it "contains messages in an array" do
-    expected = ["I", " ", "a", "m", " ", "c", "u", "r", "i", "o", "u", "s"]
-    expect(@translate_to_braille.braille_array).to eq(expected)
+  it "has attributes" do
+    expect(@translate.char_length).to be_a Integer
   end
 
-end
 
-def limits
-  frog_array = []
-  # message_array = @message_reader.split("")
-frogs = message_array.each_slice(40).map(&:join)
-  english = English.new
-  frogs.each do |frog|
-    frog_array << frog.split("")
-  end
-  frog_array
-  binding.pry
-end
+
 end
